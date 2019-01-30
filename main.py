4f091b303b63973a8c49765689b6aaef992f445b
syed <syedafshan11@gmail.com>
2019-01-31 01:24:42 +0530

    make it better

---
+++ main.py
@@ -0,0 +1,195 @@
+import pyscreenshot as ImageGrab
+import cv2
+import numpy as np
+import ast
+import re
+import pyautogui
+import time
+import json
+import requests
+a = np.zeros((6, 6))
+blocks = {}
+allBoards = [];
+parent = {}
+queue = [];
+block_number = 1
+
+def arrayToBlock(arr, num):
+    res = {};
+    for var in range(6):
+        for var2 in range(6):
+            if(len(res) == num):
+                return res
+            for key in range(num):
+                if(arr[var][var2] == key+1 and not key+1 in res):
+                    res[key+1] = [[var, var2], blocks[key+1][1], blocks[key+1][2],blocks[key+1][3]]
+
+def whichNumberMovedWhere(b1, b2):
+	b1=re.sub('\[ +', '[', b1.strip())
+	b1=re.sub('[,\s]+', ', ', b1)
+	b1 = np.array(ast.literal_eval(b1))
+	b2=re.sub('\[ +', '[', b2.strip())
+	b2=re.sub('[,\s]+', ', ', b2)
+	b2 = np.array(ast.literal_eval(b2))
+
+	b1=arrayToBlock(b1, block_number-1)
+	b2=arrayToBlock(b2, block_number-1)
+	# print(b1)
+	
+	nmoved = 1
+	for var in range(block_number-1):
+		if(b1[var+1] != b2[var+1]):
+			nmoved = var+1
+	if(b1[nmoved][1] == 'h'):
+		if(b2[nmoved][0][1] > b1[nmoved][0][1]):
+			where = 'r'
+		else:
+			where = 'l'
+	else:
+		if(b2[nmoved][0][0] > b1[nmoved][0][0]):
+			where = 'd'
+		else:
+			where = 'u'
+	return (nmoved,where,b1[nmoved][3][0], b1[nmoved][3][1])
+
+
+def mapper(rect, block_number):
+	x = rect[0]
+	y = rect[1]
+	w = rect[2]
+	h = rect[3]
+	allign = 'h'
+	size = 's'
+	if(y>-10 and y< 40):
+		y_axis = 0
+	elif(y > 40 and y < 100):
+		y_axis = 1
+	elif(y > 100 and y < 160):
+		y_axis = 2
+	elif(y > 160 and y < 220):
+		y_axis = 3
+	elif(y > 220 and y < 280):
+		y_axis = 4
+	else:
+		y_axis = 5
+	if(x>-10 and x< 40):
+		x_axis = 0
+	elif(x > 40 and x < 100):
+		x_axis = 1
+	elif(x > 100 and x < 160):
+		x_axis = 2
+	elif(x > 160 and x < 220):
+		x_axis = 3
+	elif(x > 220 and x < 280):
+		x_axis = 4
+	else:
+		x_axis = 5
+	if(w > h):
+
+		allign = 'h'
+
+		if(w>170):
+			size = 'l'
+			a[y_axis][x_axis] = block_number
+			a[y_axis][x_axis+1] = block_number
+			a[y_axis][x_axis+2] = block_number
+		else:
+			size = 's'
+			a[y_axis][x_axis] = block_number
+			a[y_axis][x_axis+1] = block_number
+	else:
+		allign = 'v'
+		if(h>170):
+			size = 'l'
+			a[y_axis][x_axis] = block_number
+			a[y_axis+1][x_axis] = block_number
+			a[y_axis+2][x_axis] = block_number
+		else:
+			size = 's'
+			a[y_axis][x_axis] = block_number
+			a[y_axis+1][x_axis] = block_number
+
+	blocks[block_number] = [[y_axis, x_axis], allign, size, [x,y]];
+
+def boxes_thresholding(img, b, g, r):
+    lower = np.array([0, 0, 0])
+    upper = np.array([b,g,r])
+    shapeMask = cv2.inRange(img, lower, upper)
+    return shapeMask
+
+def callServer(block_number, array, block):
+    params = {}
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    payload = {
+        'block_number': block_number,
+        'array': array,
+        'block': block
+    }
+    url = 'http://127.0.0.1:5000/api/v1/solutionFinder'
+    response = requests.post(url, headers=headers, params=params,
+                             data=json.dumps(payload))
+    response.raise_for_status()
+    return response.json() 
+
+if __name__ == '__main__':
+	time.sleep(2)
+	# to be used later
+	# # part of the screen
+	# img = ImageGrab.grab(bbox=(524, 304, 914, 695)) # X1,Y1,X2,Y2
+	# img = pyautogui.screenshot(region=(524,304, 390, 390	)) # X1,Y1,X2,Y2
+	# img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+	# cv2.imshow("tttt", img)
+	# cv2.waitKey(0)
+	# im.show()
+	# #-#
+
+	kernel = np.ones((5,5),np.uint8)
+
+	img=cv2.imread('sample.png')
+
+	threshold_boxes = boxes_thresholding(img, 0, 124, 226)
+
+	dilated_boxes = cv2.dilate(threshold_boxes,kernel,iterations = 1)
+
+	 # Find contours for detected portion of the image
+	cnts, hierarchy = cv2.findContours(dilated_boxes.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	rects = []
+
+	for c in cnts:
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+		x, y, w, h = cv2.boundingRect(approx)
+		if h >= 15:
+			# if height is enough
+			# create rectangle for bounding
+			rect = (x, y, w, h)
+			rects.append(rect)
+			cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1);
+			# cv2.imshow("tttt", img)
+			# cv2.waitKey(0)
+			mapper(rect, block_number)
+			block_number +=1;
+	print(np.array2string(a))
+	result = callServer(block_number, a.tolist(), str(blocks))
+	
+	# for var in range(len(result)):
+	# 	print(np.asarray(result[var]))
+	# print(a)
+	for var in range(len(result)-1):
+		num, dire, x, y = whichNumberMovedWhere(result[var], result[var+1])
+		print (num, dire)
+		# x = x+524
+		# y = y+307
+		# pyautogui.moveTo(x+20, y+20)
+		# time.sleep(2)
+
+		# if(dire == "l"):
+		# 	pyautogui.dragTo(x-400, y, button='left') 
+		# if(dire == "r"):
+		# 	pyautogui.dragTo(x+400, y, button='left') 
+		# if(dire == "u"):
+		# 	pyautogui.dragTo(x, y-400, button='left') 
+		# if(dire == "d"):
+		# 	pyautogui.dragTo(x, y+400, button='left') 
---
+++ webserver.py
@@ -0,0 +1,192 @@
+import flask
+from flask import request, jsonify
+import numpy as np
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+import ast
+allBoards = [];
+parent = {}
+queue = [];
+
+
+def arrayToBlock(arr, num, blocks):
+    res = {};
+    for var in range(6):
+        for var2 in range(6):
+            if(len(res) == num):
+                return res
+            for key in range(num):
+                if(arr[var][var2] == key+1 and not key+1 in res):
+                    res[key+1] = [[var, var2], blocks[key+1][1], blocks[key+1][2],blocks[key+1][3]]
+
+def backtrace(start, end):
+    path = [end]
+    while path[-1] != start:
+        path.append(parent[path[-1]])
+    path.reverse()
+    return path
+
+def alreadyExists(board):
+    for var in range(len(allBoards)):
+        if (np.array_equal(board, allBoards[var])):
+            return True;
+
+    return False
+
+#this return all possible boards after all possbile moves of given block
+def allPossibleMoves(number, block, arr):
+    # print(number, block, arr)
+    allPossibilities = [];
+    #if block is horizontal 
+    if(block[1] == 'h'):
+        #if block size is small:
+        if(block[2] == 's'):
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move left
+
+            while(j-1>=0 and b[i][j-1] == 0):
+                b[i][j+1] = 0
+                b[i][j-1] = number
+                j-=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+            b=np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move right
+
+            while(j+2<6 and b[i][j+2] == 0):
+                b[i][j] = 0
+                b[i][j+2] = number
+                j+=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+        #if block size is large:
+        if(block[2] == 'l'):
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            # print(i,j)
+            ifnew = False
+            #check possible to move left
+            while(j-1>=0 and b[i][j-1] == 0):
+                b[i][j+2] = 0
+                b[i][j-1] = number
+                j-=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move right
+            while(j+3<6 and b[i][j+3] == 0):
+                b[i][j] = 0
+                b[i][j+3] = number
+                j+=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b) ==False):
+                allPossibilities.append(b)
+    #if block is vertical
+    if(block[1] == 'v'):
+        #if block size is small:
+        if(block[2] == 's'):
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move up
+            while(i-1>=0 and b[i-1][j] == 0):
+                b[i+1][j] = 0
+                b[i-1][j] = number
+                i-=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+            b=np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move down
+            while(i+2<6 and b[i+2][j] == 0):
+                b[i][j] = 0
+                b[i+2][j] = number
+                i+=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+        #if block size is large:
+        if(block[2] == 'l'):
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move up
+            while(i-1>=0 and b[i-1][j] == 0):
+                b[i+2][j] = 0
+                b[i-1][j] = number
+                i-=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b)==False):
+                allPossibilities.append(b)
+            b = np.copy(arr)
+            i = block[0][0]
+            j = block[0][1]
+            ifnew = False
+            #check possible to move down
+
+            while(i+3<6 and b[i+3][j] == 0):
+                b[i][j] = 0
+                b[i+3][j] = number
+                i+=1;
+                ifnew = True
+            if(ifnew and np.array_equal(arr, b) ==False):
+                allPossibilities.append(b)
+    return np.array(allPossibilities)
+
+def solutionFinder(block_number, a, blocks):
+    for var in range(block_number-1):
+        queue.append([var+1,blocks,a])
+    allBoards.append(a)
+    while(len(queue)!=0):
+
+        tempNumber = queue.pop(0);
+        # print (tempNumber)
+        ar = allPossibleMoves(tempNumber[0], tempNumber[1][tempNumber[0]], tempNumber[2]);
+
+        for var in range(len(ar)):
+
+            if(not alreadyExists(ar[var])):
+                tempArrayBlock = arrayToBlock(ar[var], block_number-1, blocks)
+
+                allBoards.append(ar[var])
+                parent[np.array2string(ar[var])] = np.array2string(tempNumber[2]);
+                #end condition
+                if(ar[var][2][4] !=0 and ar[var][2][5]!=0 and ar[var][2][5] == ar[var][2][4]):
+                    return backtrace(np.array2string(a), np.array2string(ar[var])) 
+                for subvar in range(block_number-1):
+                    queue.append([subvar+1, tempArrayBlock, ar[var]])
+
+@app.route('/api/v1/solutionFinder', methods=['POST'])
+def api_id():
+    req_data = request.get_json()
+
+    block_number = req_data['block_number']
+    array = np.array(req_data['array'])
+    block = ast.literal_eval(req_data['block'])
+
+    # print(type(block))
+    # print(type(array))
+    # print(block_number, array, block)
+    print(solutionFinder(block_number, array, block))
+    return (jsonify(solutionFinder(block_number, array, block)))
+    
+
+app.run()
